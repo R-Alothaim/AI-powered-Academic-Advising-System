6cea595d3c42c2463290d6868b98394949b454a7
Ralothaim <ralothaim@outlook.com>
2023-01-13 16:41:04 +0300

    eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,8 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+export default [
+  { ignores: ['dist'] },
+  {
