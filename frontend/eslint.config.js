a5bf4fcd2a7723001c6aef66065768f85a19ceec
Ralothaim <ralothaim@outlook.com>
2023-01-15 11:30:52 +0300

    eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -14,3 +14,12 @@ export default [
     },
     plugins: {
       'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
