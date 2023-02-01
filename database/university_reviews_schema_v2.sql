714bab4b8d6c2cf724631a22f2209d421ff2a912
Ralothaim <ralothaim@outlook.com>
2023-02-01 12:44:51 +0300

    tables

---
+++ database/university_reviews_schema_v2.sql
@@ -0,0 +1,8 @@
+-- Warning: column statistics not supported by the server.
+-- MySQL dump 10.13  Distrib 8.4.0, for macos13.2 (x86_64)
+--
+-- Host: localhost    Database: university_reviews
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
