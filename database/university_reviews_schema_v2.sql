e956bed8eaff4c5ab54b24fbe31790dcde88c39a
Ralothaim <ralothaim@outlook.com>
2023-02-11 10:06:35 +0300

    db stuff

--- database/university_reviews_schema_v2.sql
+++ database/university_reviews_schema_v2.sql
@@ -132,3 +132,11 @@ BEGIN
         updated_at = CURRENT_TIMESTAMP
     WHERE id = NEW.chat_id;
 END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
