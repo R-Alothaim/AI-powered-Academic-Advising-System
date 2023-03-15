78b0e915dfe91ae4dde42806d448baaf2242837f
Ralothaim <ralothaim@outlook.com>
2023-03-15 15:46:54 +0300

    stuff

---
+++ frontend/src/components/AuthBackground.jsx
@@ -0,0 +1,7 @@
+export default function AuthBackground() {
+  return (
+    <>
+      <div className="bg" />
+      <div className="orb orb1" />
+      <div className="orb orb2" />
+      <div className="orb orb3" />
