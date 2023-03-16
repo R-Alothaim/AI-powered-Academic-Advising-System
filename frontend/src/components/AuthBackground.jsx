a0423b6611e5d7327a663d75593ad64742b94ddd
Ralothaim <ralothaim@outlook.com>
2023-03-16 13:03:37 +0300

    wip

--- frontend/src/components/AuthBackground.jsx
+++ frontend/src/components/AuthBackground.jsx
@@ -5,3 +5,12 @@ export default function AuthBackground() {
       <div className="orb orb1" />
       <div className="orb orb2" />
       <div className="orb orb3" />
+
+      <svg
+        className="network-svg"
+        viewBox="0 0 1440 900"
+        preserveAspectRatio="xMidYMid slice"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g stroke="#3A6AE8" strokeWidth="0.7" fill="none">
+          <line x1="80" y1="120" x2="200" y2="220" />
