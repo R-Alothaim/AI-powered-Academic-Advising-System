2523e4bf045fecc8160a8c08c54198c455b722f4
Ralothaim <ralothaim@outlook.com>
2023-03-21 17:40:05 +0300

    stuff

--- frontend/src/components/AuthBackground.jsx
+++ frontend/src/components/AuthBackground.jsx
@@ -70,3 +70,11 @@ export default function AuthBackground() {
       <div className="brand">
         <div className="brand-icon">
           <svg viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M11 2L13.5 8H20L14.5 12L16.5 18L11 14L5.5 18L7.5 12L2 8H8.5L11 2Z"
+              stroke="#EAD27A"
+              strokeWidth="1.2"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
