f1dca37621f05bbb60dde571c17ff58956985ecf
Ralothaim <ralothaim@outlook.com>
2023-03-26 16:21:59 +0300

    auth logic

--- backend/auth_router.py
+++ backend/auth_router.py
@@ -7,3 +7,12 @@ import secrets
 import hashlib
 import logging
 from datetime import datetime, timedelta, timezone
+
+import bcrypt
+
+from fastapi import APIRouter, Depends, HTTPException, Request
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import Session
+
+logger = logging.getLogger(__name__)
