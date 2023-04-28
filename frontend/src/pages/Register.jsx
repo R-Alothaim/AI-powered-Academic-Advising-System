7df418888c9b293468463db35658d8a4354fc93b
Ralothaim <ralothaim@outlook.com>
2023-04-28 19:42:39 +0300

    stuff

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -16,3 +16,14 @@ export default function Register() {
   const [loading, setLoading] = useState(false);
 
   const validate = () => {
+    const errs = [];
+    if (!name.trim()) errs.push('Full name is required');
+    if (!email.includes('@university.edu.sa')) errs.push('Only @university.edu.sa emails are allowed');
+    if (password.length < 8) errs.push('Password must be at least 8 characters');
+    if (!/[A-Z]/.test(password)) errs.push('Password must contain an uppercase letter');
+    if (!/[a-z]/.test(password)) errs.push('Password must contain a lowercase letter');
+    if (!/\d/.test(password)) errs.push('Password must contain a digit');
+    if (!/[^A-Za-z0-9]/.test(password)) errs.push('Password must contain a special character');
+    if (password !== rePassword) errs.push('Passwords do not match');
+    return errs;
+  };
