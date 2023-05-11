cfc7837e48c80db43f86160e32aeed72139e2344
Ralothaim <ralothaim@outlook.com>
2023-05-11 12:53:35 +0300

    wip

--- frontend/src/pages/OtpVerify.jsx
+++ frontend/src/pages/OtpVerify.jsx
@@ -6,3 +6,12 @@ import AuthBackground from '../components/AuthBackground';
 import '../styles/auth-bg.css';
 
 export default function OtpVerify() {
+  const { setSession } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const email = location.state?.email || '';
+
+  const [code, setCode] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [loading, setLoading] = useState(false);
