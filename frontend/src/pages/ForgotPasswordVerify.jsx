bfd5db61dd5e8aed35d3d78db6c9a0d28237f41f
Ralothaim <ralothaim@outlook.com>
2023-05-21 23:36:56 +0300

    wip

--- frontend/src/pages/ForgotPasswordVerify.jsx
+++ frontend/src/pages/ForgotPasswordVerify.jsx
@@ -8,3 +8,12 @@ export default function ForgotPasswordVerify() {
   const navigate = useNavigate();
   const location = useLocation();
   const email = location.state?.email || '';
+
+  const [otp, setOtp] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setLoading(true);
