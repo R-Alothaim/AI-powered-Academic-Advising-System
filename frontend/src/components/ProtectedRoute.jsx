701501825a48c246870db7dc1ac402eb269c202f
Ralothaim <ralothaim@outlook.com>
2023-05-31 12:31:33 +0300

    updates

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
