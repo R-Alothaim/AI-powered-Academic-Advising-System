a1c6cd31f4b8aabdad90f007ed22a49939789d18
Ralothaim <ralothaim@outlook.com>
2023-06-02 09:49:43 +0300

    wip

---
+++ frontend/src/components/ErrorBoundary.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
