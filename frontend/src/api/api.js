ffd4045357b9ec4b64c73716c7229f41750d5799
Ralothaim <ralothaim@outlook.com>
2023-06-03 10:25:47 +0300

    api layer

---
+++ frontend/src/api/api.js
@@ -0,0 +1,8 @@
+const API_BASE = '/api';
+
+async function request(path, options = {}) {
+  const token = localStorage.getItem('token');
+  const headers = { 'Content-Type': 'application/json', ...options.headers };
+  if (token) headers['Authorization'] = `Bearer ${token}`;
+
+  const res = await fetch(`${API_BASE}${path}`, { ...options, headers });
