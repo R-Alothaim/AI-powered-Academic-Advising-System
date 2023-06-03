325eb664fa37e396d0fef36324190ebe1635a070
Ralothaim <ralothaim@outlook.com>
2023-06-03 23:28:14 +0300

    api layer

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -6,3 +6,15 @@ async function request(path, options = {}) {
   if (token) headers['Authorization'] = `Bearer ${token}`;
 
   const res = await fetch(`${API_BASE}${path}`, { ...options, headers });
+
+  if (res.status === 401) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    window.location.href = '/login';
+    throw new Error('Unauthorized');
+  }
+
+  if (!res.ok) {
+    const body = await res.json().catch(() => ({}));
+    throw new Error(body.detail || body.message || `Request failed (${res.status})`);
+  }
