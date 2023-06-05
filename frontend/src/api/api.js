8a1ff514f495dc212ff561e9986a80847d1d2ee9
Ralothaim <ralothaim@outlook.com>
2023-06-05 15:16:47 +0300

    api layer

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -28,3 +28,12 @@ export const auth = {
 
   register: (name, email, password) =>
     request('/auth/register', { method: 'POST', body: JSON.stringify({ name, email, password }) }),
+
+  verifyOtp: (email, code) =>
+    request('/auth/verify-otp', { method: 'POST', body: JSON.stringify({ email, code }) }),
+
+  resendOtp: (email) =>
+    request('/auth/resend-otp', { method: 'POST', body: JSON.stringify({ email }) }),
+
+  forgotPassword: (email) =>
+    request('/auth/forgot-password', { method: 'POST', body: JSON.stringify({ email }) }),
