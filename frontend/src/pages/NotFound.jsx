c36f1cd26c47854265d1757fe8002aa1ea3ad611
Ralothaim <ralothaim@outlook.com>
2023-07-11 09:28:57 +0300

    updates

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -6,3 +6,11 @@ export default function NotFound() {
   const dest = user ? '/dashboard/chats' : '/login';
 
   return (
+    <div style={{
+      minHeight: '100vh', display: 'flex', flexDirection: 'column',
+      alignItems: 'center', justifyContent: 'center', background: '#0A1628',
+      color: '#E2E8F0', fontFamily: 'Inter, sans-serif', padding: 32,
+    }}>
+      <h1 style={{
+        fontFamily: 'Playfair Display, Georgia, serif', fontSize: 72,
+        color: '#EAD27A', marginBottom: 8,
