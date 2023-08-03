3cecb5740f7332162ec1c5968401269252bc5f66
Ralothaim <ralothaim@outlook.com>
2023-08-03 17:44:52 +0300

    updates

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -7,3 +7,12 @@ import { users } from '../api/api';
 import '../styles/profile.css';
 
 const PALETTE = ['#7c3aed', '#0ea5e9', '#10b981', '#f59e0b', '#ef4444', '#8b5cf6'];
+
+function getInitials(name, email) {
+  const n = (name || '').trim();
+  if (n) {
+    const parts = n.split(/\s+/);
+    return parts.slice(0, 2).map((p) => p.charAt(0).toUpperCase()).join('');
+  }
+  return (email || '').slice(0, 2).toUpperCase();
+}
