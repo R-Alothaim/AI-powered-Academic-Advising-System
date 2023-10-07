6aeb61d7cf83a41355e1c458871080e707de2771
Ralothaim <ralothaim@outlook.com>
2023-10-08 00:44:52 +0300

    calendar api

--- backend/calendar_router.py
+++ backend/calendar_router.py
@@ -17,3 +17,12 @@ CACHE_DURATION = 3600
 def _cache_path(year: str, lang: str) -> Path:
     CACHE_DIR.mkdir(parents=True, exist_ok=True)
     return CACHE_DIR / f"calendar_{year}_{lang}.json"
+
+def _read_cache(year: str, lang: str):
+    p = _cache_path(year, lang)
+    if p.exists() and (datetime.now().timestamp() - p.stat().st_mtime) < CACHE_DURATION:
+        try:
+            return json.loads(p.read_text(encoding="utf-8"))
+        except Exception:
+            pass
+    return None
