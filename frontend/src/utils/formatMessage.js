bb668eeae8b8d0eed473468688f94f06f31a0d9c
Ralothaim <ralothaim@outlook.com>
2023-11-28 17:14:11 +0300

    wip

--- frontend/src/utils/formatMessage.js
+++ frontend/src/utils/formatMessage.js
@@ -6,3 +6,12 @@ export default function formatMessage(text) {
   text = text.replace(/\s*\n\s*\.\s*$/gm, '.').replace(/\s*\n\s*\.\s*\n/gm, '.\n');
 
   if (text.includes('<table') || text.includes('<tr') || text.includes('<td')) {
+    text = text
+      .replace(/>\s+</g, '><')
+      .replace(/(<table[^>]*>)/g, '<div style="margin:12px 0">$1')
+      .replace(/(<\/table>)/g, '$1</div>');
+    return DOMPurify.sanitize(text);
+  }
+
+  text = text.replace(/^\* (.+)$/gm, '<li>$1</li>').replace(/^- (.+)$/gm, '<li>$1</li>');
+  text = text.replace(/(<li>.*<\/li>\s*)+/gs, (m) => '<ul>' + m + '</ul>');
