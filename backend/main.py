02cacad951160d3348fe49791102792c6812a4b9
Ralothaim <ralothaim@outlook.com>
2023-12-02 23:55:15 +0300

    fastapi

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,11 @@ from contextlib import asynccontextmanager
 from datetime import datetime, timezone
 
 import httpx
+from dotenv import load_dotenv
+from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
+from pydantic import BaseModel
+
+from ultimate_rag_system import UltimateRAGSystem
